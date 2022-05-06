dceb6717d87d6446927afee84af0993030988c77
marwa-kilani <kilani.marwa93@gmail.com>
2022-05-06 10:57:50 +0100

    adding checkpoint react

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import './App.css';
+import { Button } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+<Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address:</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password:</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+
+    {/*<header className="App-header"> 
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>*/}
+    </div> 
+  );
+}
+
+export default App;
